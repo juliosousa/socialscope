2edcc513bb5bc85246baf271a2c64e9950480520
INGOH Laboratório <marketing@ingoh.com.br>
2017-05-09 08:22:09 -0300

    acrecentado token vitalicio do facebook

--- app/controllers/fetch.rb
+++ app/controllers/fetch.rb
@@ -1,25 +1,26 @@
 Socialscopeme::App.controllers :fetch do
-  
+
   post :index do
 
     ap params
-    
-    
-    break 
+
+
+    break
 
     agent = Mechanize.new
 
     ##Colocar apenas o nome da página
-    @link_page = params[:page]
+    @link_page = params['url'].split('/')[3]
     ##Não mexer
     # csv = CSV.new(open("files/2017/links_#{@link_page}.csv", 'wb'))
     # csv << %w(CREATED LINK LIKES COMMENTS)
-    
+
     @count = 0
 
     #TODO: Criar um atenticador para API do Facebook
-    @graph = Koala::Facebook::API.new("EAACEdEose0cBAMxdV1XU3SZA8ZCNHnZB25oyT5vCIqlbwcUmb8Vv6bMs1O82Wa0G0BRxgrVIcmiZA6tAyswHAeLOJ4s0X8jZC0mtKoqcGnfkZANPGIGfCc73SN1ofwUPGmFRnZAjZAhmZB6eOKt21onYVvwJQ5cG04tVq66OwRC5ydtGhQ5JGbXVa7ytFKfIfL9MZD")
-    
+    #O token abaixo nao tem validade, pode ser utilizado futuramente em producao.
+    @graph = Koala::Facebook::API.new("EAALQhxGV2qYBALElsXTLBIROBjTFvFYRTnI01ajJoCyghZCHZAdcpSi5Iu8sD3xgCDKHCxQuiA4Ea09sdmO9qTHfFY06ZAicC0Ln7skQBoZCYCn9cSPYOWl7EiZAKvGO3a1WylmFpR6c2MiM4ONFdPGHVWZAePUTgZD")
+
     page = @graph.get_connection(@link_page, 'posts',{
         limit: 99,
         fields: ['likes.limit(0).summary(true)','comments.limit(0).summary(true)']
